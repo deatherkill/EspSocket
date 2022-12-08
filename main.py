8d25b82dd6246b95625800a2c2ee1a717f64b430
EugenePetrykeiev <eugenepetrikeev@gmail.com>
2022-12-08 03:47:57 +0200

    Fixed time, erase comments

--- main.py
+++ main.py
@@ -13,11 +13,10 @@ s.listen(0)
 
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
-print(f"Hostname: {hostname}")
-print(f"IP Address: {ip_address}")
+
 
 off_time = 0
-delay_time = 4
+delay_time = 30
 cur_time = time.time()
 state = False
 prev_state = False
@@ -39,8 +38,7 @@ def socks():
 
 
 def timer():
-    # print(off_time)
-    global prev_state
+    global prev_state, cur_time, state
     while True:
         cur_time = int(time.time())
         state = not (cur_time > off_time + delay_time)
@@ -58,38 +56,9 @@ def timer():
 
 
 if __name__ == "__main__":
+    print(f"Hostname: {hostname}")
+    print(f"IP Address: {ip_address}")
     t1 = threading.Thread(target=timer)
     t2 = threading.Thread(target=socks)
     t1.start()
     t2.start()
-# while True:
-#     # client, addr = s.accept()
-#     try:
-#         client, addr = s.accept()
-#         client.settimeout(1)
-#         content = client.recv(1024)
-#         print(content)
-#     except:
-#         print('Not connected')
-
-# while True:
-#     content = s.recv(1024)
-#     if len(content) == 0:
-#         break
-#     if str(content, 'utf-8') == '\r\n':
-#         continue
-#     else:
-#         off_time = int(time.time())
-#         state = True
-#
-# if int(time.time()) > off_time + delay_time:
-#     state = False
-#
-# if state:
-#     print('On', dt.now().strftime('%Y-%m-%d, %H:%M:%S'))
-# else:
-#     print('Off', dt.now().strftime('%Y-%m-%d, %H:%M:%S'))
-# client.close()
-
-# start_time = int(time.time())
-# while True:
