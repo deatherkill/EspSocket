cb24c343cc80f78e2c2b48d9f6cade3c7fbe303a
EugenePetrykeiev <eugenepetrikeev@gmail.com>
2022-12-08 16:23:49 +0200

    Server socket connect

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 import socket
+import sys
 import time
 import telebot
 from datetime import datetime as dt
 import threading
-
-bot = telebot.TeleBot("5841428040:AAGRVyc-EVDY1cFPHv-NfzQRATzyDJWaE3k", threaded=True)
-chat_id = 648624553
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0', 8090))
-s.listen(0)
-
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
-
-
-off_time = 0
-delay_time = 30
-cur_time = time.time()
-state = False
-prev_state = False
 
 
 def socks():
@@ -31,9 +15,8 @@ def socks():
             # print(content)
             global off_time
             off_time = int(time.time())
-            # print(off_time)
-        except:
-            print('Not connected')
+        except socket.error as e:
+            print('Socket Error:', e)
             client.close()
 
 
@@ -47,16 +30,31 @@ def timer():
             if state:
                 print('On', dt.now().strftime('%Y-%m-%d, %H:%M:%S'))
                 timestamp = dt.now().strftime('%Y-%m-%d, %H:%M:%S')
-                bot.send_message(chat_id=chat_id, text=f'🟢Power is ON at: {timestamp}', timeout=5)
+                bot.send_message(chat_id=chat_id, text=f'🟢 Power is ON at: {timestamp}', timeout=5)
             else:
                 print('Off', dt.now().strftime('%Y-%m-%d, %H:%M:%S'))
                 timestamp = dt.now().strftime('%Y-%m-%d, %H:%M:%S')
-                bot.send_message(chat_id=chat_id, text=f'🟠Power is OFF at: {timestamp}', timeout=5)
+                bot.send_message(chat_id=chat_id, text=f'🔴 Power is OFF at: {timestamp}', timeout=5)
         prev_state = state
 
 
 if __name__ == "__main__":
-    print(f"Hostname: {hostname}")
+    bot = telebot.TeleBot("5841428040:AAGRVyc-EVDY1cFPHv-NfzQRATzyDJWaE3k", threaded=True)
+    chat_id = 648624553
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as e:
+        print('Socket connection error')
+        sys.exit(1)
+    s.bind(('0.0.0.0', 8090))
+    s.listen(0)
+    ip_address = socket.gethostbyname('185.25.118.34')
+    off_time = 0
+    delay_time = 30
+    cur_time = time.time()
+    state = False
+    prev_state = False
+
     print(f"IP Address: {ip_address}")
     bot.send_message(chat_id=chat_id, text=f'{ip_address}')
     t1 = threading.Thread(target=timer)
