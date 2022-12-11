8cce0fdee79395a9479e34cd10bd2159fbe2ffb0
EugenePetrykeiev <eugenepetrikeev@gmail.com>
2022-12-11 14:55:57 +0200

    Add credits, minor changes

--- main.py
+++ main.py
@@ -4,14 +4,29 @@ import time
 import telebot
 from datetime import datetime as dt
 import threading
+import json
+
+with open('creds.json') as f:
+    data = json.load(f)
+
+bot = telebot.TeleBot(data['bot'], threaded=True)
+content = ''
+server_status = ''
+global ip_address
+port = data['port']
+
+group_id = data['group_id']
+chat_id = data['chat_id']
+local = data['local_ip']
 
 
 def socks():
     while True:
+        global content
         try:
             client, addr = s.accept()
             client.settimeout(1)
-            # content = client.recv(1024).decode()
+            content = client.recv(1024).decode()
             # print(content)
             global off_time
             off_time = int(time.time())
@@ -21,34 +36,48 @@ def socks():
 
 
 def timer():
-    global prev_state, cur_time, state
+    global prev_state, cur_time, state, server_status
     while True:
         cur_time = int(time.time())
         state = not (cur_time > off_time + delay_time)
         # print(state, prev_state)
+        power_check = content == 'Power is OK'
         if state is not prev_state:
-            if state:
+            if state and power_check:
                 print('On', dt.now().strftime('%Y-%m-%d, %H:%M:%S'))
                 timestamp = dt.now().strftime('%Y-%m-%d, %H:%M:%S')
-                bot.send_message(chat_id=chat_id, text=f'🟢 Power is ON at: {timestamp}', timeout=5)
+                bot.send_message(chat_id=group_id, text=f'🟢 Power is ON at: {timestamp}', timeout=5)
+                server_status = '🟢'
             else:
                 print('Off', dt.now().strftime('%Y-%m-%d, %H:%M:%S'))
                 timestamp = dt.now().strftime('%Y-%m-%d, %H:%M:%S')
-                bot.send_message(chat_id=chat_id, text=f'🔴 Power is OFF at: {timestamp}', timeout=5)
+                bot.send_message(chat_id=group_id, text=f'🔴 Power is OFF at: {timestamp}', timeout=5)
+                server_status = '🔴'
         prev_state = state
 
 
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f"Bot is working. Status: {server_status}")
+
+
+@bot.message_handler(commands=['ip'])
+def send_welcome(message):
+    bot.reply_to(message, text=f'{ip_address}:{port}')
+
+
 if __name__ == "__main__":
-    bot = telebot.TeleBot("5841428040:AAGRVyc-EVDY1cFPHv-NfzQRATzyDJWaE3k", threaded=True)
-    chat_id = 648624553
+
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error as e:
         print('Socket connection error')
         sys.exit(1)
-    s.bind(('0.0.0.0', 8090))
+    s.bind(('0.0.0.0', port))
     s.listen(0)
-    ip_address = socket.gethostbyname('185.25.118.34')
+    hostname = socket.gethostname()
+
+    ip_address = socket.gethostbyname(hostname)
     off_time = 0
     delay_time = 30
     cur_time = time.time()
@@ -56,8 +85,8 @@ if __name__ == "__main__":
     prev_state = False
 
     print(f"IP Address: {ip_address}")
-    bot.send_message(chat_id=chat_id, text=f'{ip_address}')
     t1 = threading.Thread(target=timer)
     t2 = threading.Thread(target=socks)
     t1.start()
     t2.start()
+    bot.infinity_polling()
